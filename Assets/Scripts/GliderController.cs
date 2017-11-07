936123ba48e77c14cee9175650105c050b3c6b81
23448 <23448@ma-web.nl>
2017-11-07 22:13:45 +0100

    Add files via upload

---
+++ Assets/Scripts/GliderController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GliderController : MonoBehaviour {
+
+    //Ik had nog meer code maar had het weg moeten doen wegens teveel fouten in de code.
+
+    public int gridWidth;
+    public int gridHeighth;
+    //public Texture2DArray grid = new Texture2DArray(256, 256, 1, TextureFormat.ARGB4444, false);
+    public GameObject[,][] Glider = new GameObject[1,2][];
+
+
+void Start()
+    {
+
+    }
+
+
+
+}
+
+
